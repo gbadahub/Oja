f08881b8137d0adaee26230f8329254b7d4b755e
PandaBurglar <zane.bokhari@gmail.com>
2022-02-13 18:11:40 -0500

    update prodct id numbers to start after 18 for shoes

--- db/seeds/product_seeds/02_products_shoes_seeds.sql
+++ db/seeds/product_seeds/02_products_shoes_seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO products (id,name,price,img,description, is_available,category_id,
 user_id)
-VALUES (1,"Rockstud Caged 100 Leather Valentino Pumps", "50", "Oja/public/img/bags/shoes/valentino-rockstud-caged-100-leather-pumps.jpg","
+VALUES (19,"Rockstud Caged 100 Leather Valentino Pumps", "50", "Oja/public/img/bags/shoes/valentino-rockstud-caged-100-leather-pumps.jpg","
 - US 8
 - Rockstud Caged 100 Leather Ankle Strap Pump In Black Leather With Rockstud Trim
 - Adjustable Ankle Strap With Buckle Closure
@@ -8,11 +8,11 @@ VALUES (1,"Rockstud Caged 100 Leather Valentino Pumps", "50", "Oja/public/img/ba
 - Smooth Leather Sole
 - 4.3In Heel
 MADE IN ITALY", true, 2, 2),
-(2,"Grey Leather Rockstud Pointed-Yoe Valentino Heels", "45", "Oja/public/img/bags/shoes/valentino-grey-leather-rockstud-pointed-toe.jpg","
+(21,"Grey Leather Rockstud Pointed-Yoe Valentino Heels", "45", "Oja/public/img/bags/shoes/valentino-grey-leather-rockstud-pointed-toe.jpg","
 - US 6
 Gently Used - EXTERIOR: Scuffs on toe part SOLE: Visible signs of use INSOLE: Minor toe marks and wrinkles HEEL: Visible scuffs and tear on right foot LINING /Heel Compartment: Minor wrinkles and scuffs.
 When considering Valentino, three words come to mind: luxurious, bold, and iconic. These gorgeous sandals are crafted from leather, and the sleek silhouette is adorned with the carefully placed Rockstuds. They are complete with pointed toes and stiletto heels for the right amount of elevation.  SKU - 557880", true, 2, 4),
-(3,"Black Leather Valentino Ankle Boots", "30", "Oja/public/img/bags/shoes/valentino-black-bootsbooties.jpg","
+(22,"Black Leather Valentino Ankle Boots", "30", "Oja/public/img/bags/shoes/valentino-black-bootsbooties.jpg","
 - US 9
 Black
 Rockstud Accents
