e7ce227ba6da0eecbde601f2cef0e9cb3cc70d96
PandaBurglar <zane.bokhari@gmail.com>
2022-02-13 18:07:46 -0500

    new product_seeds folder to contain seeds for bags, acessories, dresses, shoes

---
+++ db/seeds/product_seeds/02_products__bags_seeds.sql
@@ -0,0 +1,43 @@
+INSERT INTO products (id,name,price,img,description, is_available,category_id,
+user_id)
+VALUES (1,"Beige Veneta", "30", "public/img/bags/beige_bottega.png","- beige
+ - leather
+- padded design
+- gold-tone hardware
+- foldover top
+- magnetic fastening
+- adjustable shoulder strap
+- main compartment", true),
+(2,"Lime-Green Bottega Veneta", "30","public/img/bags/limegreen_bottega.png","- Lime Gree
+- leather
+- padded Intrecciato design
+- front flap closure
+- clasp fastening
+- internal zip-fastening pocket
+- adjustable shoulder strap", true),
+(3, "Purple Bottega Veneta", "25", "public/img/bags/purple_bottega.png","- purple
+- leather
+- interwoven design
+- polished finish
+- adjustable shoulder strap
+- foldover top
+- main compartment
+- internal zip pocket", true),
+(4, "Light brown with snakeskin print Fendi bag", "35", "public/img/bags/beige_fendi.png", "
+- Made in Italy
+- Python Skin
+- Detachable arm", true ),
+(5,"Fendi Baguette", "37", "public/img/bags/black_fendi.png", "- Made in Italy
+- 100% Lamb Leather
+", true ),
+(6, "Peekaboo Iconic Mini","39", "public/img/bags/classicmonogram_fendi.png" , " - Sheepskin
+- Goldtone Metal ware
+- Adjustable Strap
+- Made in Itay", true),
+(7, "Black Chanel Bag", "45", "public/img/bags/black_chanel.png", "- Classic Chanel
+- Gold and black Strap", true),
+(8, "Blue and Purple tie-dye Chanel Tote", "40", "public/img/bags/blueandpurple_tiedye_chanel.png", "
+- With Black and gold straps
+- Gold tone metal ware ", true),
+(9, "Wallet Chanel bag", "50", "public/img/bags/lightpink_chanel.png", "
+")
---
+++ db/seeds/product_seeds/02_products_shoes_seeds.sql
@@ -0,0 +1,27 @@
+INSERT INTO products (id,name,price,img,description, is_available,category_id,
+user_id)
+VALUES (1,"Rockstud Caged 100 Leather Valentino Pumps", "50", "Oja/public/img/bags/shoes/valentino-rockstud-caged-100-leather-pumps.jpg","
+- US 8
+- Rockstud Caged 100 Leather Ankle Strap Pump In Black Leather With Rockstud Trim
+- Adjustable Ankle Strap With Buckle Closure
+- Lightly Padded Leather Insole
+- Smooth Leather Sole
+- 4.3In Heel
+MADE IN ITALY", true, 2, 2),
+(2,"Grey Leather Rockstud Pointed-Yoe Valentino Heels", "45", "Oja/public/img/bags/shoes/valentino-grey-leather-rockstud-pointed-toe.jpg","
+- US 6
+Gently Used - EXTERIOR: Scuffs on toe part SOLE: Visible signs of use INSOLE: Minor toe marks and wrinkles HEEL: Visible scuffs and tear on right foot LINING /Heel Compartment: Minor wrinkles and scuffs.
+When considering Valentino, three words come to mind: luxurious, bold, and iconic. These gorgeous sandals are crafted from leather, and the sleek silhouette is adorned with the carefully placed Rockstuds. They are complete with pointed toes and stiletto heels for the right amount of elevation.  SKU - 557880", true, 2, 4),
+(3,"Black Leather Valentino Ankle Boots", "30", "Oja/public/img/bags/shoes/valentino-black-bootsbooties.jpg","
+- US 9
+Black
+Rockstud Accents
+Round-Toes
+Block Heels with Platform
+Lace-Up Closure at Uppers & Concealed Zip Closure at Sides
+Wrap-Around Straps
+Includes Dust Bag. The box got damaged and will be sold without box but I still have it if someone requires
+Fit: This style typically runs a half size to a full size small. US size 8 or 8.5
+Heels: 3 inches
+Platforms: 0.5 inches
+", true, 2, 5),
