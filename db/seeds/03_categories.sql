9be1042b63916a6d47e0edbc2b145a4f331037c4
PandaBurglar <zane.bokhari@gmail.com>
2022-02-13 18:06:13 -0500

    add seeds for categories table

---
+++ db/seeds/03_categories.sql
@@ -0,0 +1,14 @@
+INSERT INTO categories (
+ id SERIAL PRIMARY KEY NOT NULL,
+ name VARCHAR(50),
+ bags BOOLEAN,
+ dresses BOOLEAN,
+ shoes BOOLEAN, 
+ accessories BOOLEAN, 
+ watches BOOLEAN, 
+ jewellery BOOLEAN
+) 
+VALUES (1, "Bags", True, False, False, False, False, False),
+(2, "Dresses", False, True, False, False, False, False),
+(3, "Shoes", False, False, True, False, False, False),
+(4, "Accessories", False, False, False, True, False, False);
