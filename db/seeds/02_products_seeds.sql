5b16e012d01188ee296c38e7041dfdf1192b7d29
Georgina Bada <georginabada@gmail.com>
2022-02-14 14:21:47 -0500

    Seeding products

--- db/seeds/02_products_seeds.sql
+++ db/seeds/02_products_seeds.sql
@@ -1,3 +1,51 @@
 INSERT INTO products (id,name,price,img,description, is_available,category_id,
 user_id)
-VALUES (1,)
+VALUES (1,"Beige Veneta", "30", "public/img/bags/beige_bottega.png","- beige
+ - leather
+- padded design
+- gold-tone hardware
+- foldover top
+- magnetic fastening
+- adjustable shoulder strap
+- main compartment", true, 1,2),
+(2,"Lime-Green Bottega Veneta", "30","public/img/bags/limegreen_bottega.png","- Lime Gree
+- leather
+- padded Intrecciato design
+- front flap closure
+- clasp fastening
+- internal zip-fastening pocket
+- adjustable shoulder strap", true,1,2),
+(3, "Purple Bottega Veneta", "25", "public/img/bags/purple_bottega.png","- purple
+- leather
+- interwoven design
+- polished finish
+- adjustable shoulder strap
+- foldover top
+- main compartment
+- internal zip pocket", true,1,3),
+(4, "Light brown with snakeskin print Fendi bag", "35", "public/img/bags/beige_fendi.png", "
+- Made in Italy
+- Python Skin
+- Detachable arm", true ,1,3),
+(5,"Fendi Baguette", "37", "public/img/bags/black_fendi.png", "- Made in Italy
+- 100% Lamb Leather
+", true, 1,4 ),
+(6, "Peekaboo Iconic Mini","39", "public/img/bags/classicmonogram_fendi.png" , " - Sheepskin
+- Goldtone Metal ware
+- Adjustable Strap
+- Made in Itay", true,1,4),
+(7, "Black Chanel Bag", "45", "public/img/bags/black_chanel.png", "- Classic Chanel
+- Gold and black Strap", true, 1,5),
+(8, "Blue and Purple tie-dye Chanel Tote", "40", "public/img/bags/blueandpurple_tiedye_chanel.png", "
+- With Black and gold straps
+- Gold tone metal ware ", true, 1,4),
+(9, "Wallet Chanel bag", "50", "public/img/bags/lightpink_chanel.png", "
+- Pink
+- Lambskin & Silver-Tone Metal", true, 1,4),
+(10, "Prada Cleo Bag", "40", "", "
+- Brushed leather
+- Leather handle
+- Metal hardware
+- Logo on enameled metal triangle at the front
+", true, 1,1),
+(11, "Cream Prada Bag", "40", "")
