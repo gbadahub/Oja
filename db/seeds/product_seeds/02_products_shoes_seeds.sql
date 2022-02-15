fedcd6e7c7e0f2785330f4ecd59803e4249624d2
PandaBurglar <zane.bokhari@gmail.com>
2022-02-14 20:48:09 -0500

    changed price to cents

--- db/seeds/product_seeds/02_products_shoes_seeds.sql
+++ db/seeds/product_seeds/02_products_shoes_seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO products (id,name,price,img,description, is_available,category_id,
 user_id)
-VALUES (19,"Rockstud Caged 100 Leather Valentino Pumps", "50", "Oja/public/img/bags/shoes/valentino-rockstud-caged-100-leather-pumps.jpg","
+VALUES (19,"Rockstud Caged 100 Leather Valentino Pumps", "500", "Oja/public/img/bags/shoes/valentino-rockstud-caged-100-leather-pumps.jpg","
 - US 8
 - Rockstud Caged 100 Leather Ankle Strap Pump In Black Leather With Rockstud Trim
 - Adjustable Ankle Strap With Buckle Closure
@@ -8,7 +8,7 @@ VALUES (19,"Rockstud Caged 100 Leather Valentino Pumps", "50", "Oja/public/img/b
 - Smooth Leather Sole
 - 4.3In Heel
 MADE IN ITALY", true, 2, 2),
-(20,"Black Leaopard", "40", "Oja/public/img/bags/shoes/saint-laurent-leopard-black-ysl-logo-opyum-opium-110-brown-sandal-stiletto-heel-pumps.jpg","
+(20,"Black Leaopard", "400", "Oja/public/img/bags/shoes/saint-laurent-leopard-black-ysl-logo-opyum-opium-110-brown-sandal-stiletto-heel-pumps.jpg","
 - US 9.5
 Brand: Yves Saint Laurent
 Size: 39.5 (know your YSL size) 
@@ -26,7 +26,7 @@ Brand new in box, comes with box and dust bag
 100% Authentic or your money back
 Great gift
 ", true, 2, 5),
-(21,"Grey Leather Rockstud Pointed-Yoe Valentino Heels", "45", "Oja/public/img/bags/shoes/valentino-grey-leather-rockstud-pointed-toe.jpg","
+(21,"Grey Leather Rockstud Pointed-Yoe Valentino Heels", "450", "Oja/public/img/bags/shoes/valentino-grey-leather-rockstud-pointed-toe.jpg","
 - US 6
 Gently Used - EXTERIOR: Scuffs on toe part SOLE: Visible signs of use INSOLE: Minor toe marks and wrinkles HEEL: Visible scuffs and tear on right foot LINING /Heel Compartment: Minor wrinkles and scuffs.
 When considering Valentino, three words come to mind: luxurious, bold, and iconic. These gorgeous sandals are crafted from leather, and the sleek silhouette is adorned with the carefully placed Rockstuds. They are complete with pointed toes and stiletto heels for the right amount of elevation.  SKU - 557880", true, 2, 4),
@@ -43,13 +43,13 @@ Fit: This style typically runs a half size to a full size small. US size 8 or 8.
 Heels: 3 inches
 Platforms: 0.5 inches
 ", true, 2, 5),
-(23,"Saint Laurent Gold Gloss75 Pumps", "35", "Oja/public/img/bags/shoes/saint-laurent-gold-gloss75-pumps.jpg","
+(23,"Saint Laurent Gold Gloss75 Pumps", "350", "Oja/public/img/bags/shoes/saint-laurent-gold-gloss75-pumps.jpg","
 - US 8.5
 Gold Leather upper with tassel on vamp.
 Clear sides. Pointed toe. Stacked wood 3 inch heel. 
 Excellent condition with scuffs on backs of heels. Box included
 ", true, 2, 4),
-(24,"Saint Laurent Black Opyum in Leather Booties", "30", "Oja/public/img/bags/shoes/saint-laurent-black-opyum-in-leather-with-bootsbooties.jpg","
+(24,"Saint Laurent Black Opyum in Leather Booties", "300", "Oja/public/img/bags/shoes/saint-laurent-black-opyum-in-leather-with-bootsbooties.jpg","
 - US 9
 ANKLE BOOTS WITH POINTED TOE, WITH A STRUCTURED HEEL COMPRISED OF INTERLACED YSL MONOGRAM IN METAL AND A ZIPPER CLOSING ON THE SIDE.
 100% SHEEPSKIN
@@ -61,7 +61,7 @@ MADE IN ITALY
 SLIGHTLY USED WITH SCUFFS ON SOLES 
 COMES WITH BOX DUST BAG AND CARDS
 ", true, 2, 2),
-(25,"Prada Yellow Terry Cloth Black Logo Slide", "40", "Oja/public/img/bags/shoes/prada-yellow-terry-cloth-black-logo-slide-rubber-sole-flip-flop-flat-sandals.jpg","
+(25,"Prada Yellow Terry Cloth Black Logo Slide", "400", "Oja/public/img/bags/shoes/prada-yellow-terry-cloth-black-logo-slide-rubber-sole-flip-flop-flat-sandals.jpg","
 - US 8
 Prada Yellow Terry Cloth Black Logo Slide Sandal Rubber Sole Flip Flop Flat 38
 
@@ -85,7 +85,7 @@ Great gift
 I ship worldwide
 Any other questions just ask, I will be happy to answer them
 ", true, 2, 5),
-(26,"White Monolith with Pouch Boots/Booties by Prada", "30", "Oja/public/img/bags/shoes/prada-white-monolith-with-pouch-bootsbooties.jpg","
+(26,"White Monolith with Pouch Boots/Booties by Prada", "300", "Oja/public/img/bags/shoes/prada-white-monolith-with-pouch-bootsbooties.jpg","
 - US 7
 Prada Monolith boots in calf leather and nylon fabric,
 enriched by a removable buckled strap equipped with a 
@@ -93,25 +93,25 @@ detachable nylon zip pouch with snap button on the loop and
 the iconic enameled metal triangle logo.
 Painted metal eyelets and hooks, back loop, leather lining with removable insole, oversize rubber lug sole with logo on the back.
 ", true, 2, 4),
-(27,"Prada Pink Lightning Bolt Satin Pumps", "40", "Oja/public/img/bags/shoes/prada-pink-lightning-bolt-satin-pumps.jpg","
+(27,"Prada Pink Lightning Bolt Satin Pumps", "400", "Oja/public/img/bags/shoes/prada-pink-lightning-bolt-satin-pumps.jpg","
 - US 7
 Prada Lightning Bolt Satin Pump in Pink.
 Sz 7 (please know sizing for brand and style when considering). 
 Prada's pink pumps rest on a statement lightning bolt-shaped silver heel, in keeping with the drama of the -themed AW19 runway.
 They're made in Italy from satin with a demure round toe and are lined in supple leather. Light runs in satin upper from display and handling. Overall Excellent Like New Condition.
 ", true, 2, 5),
-(29,"Jimmy Choo Dark Blue Slingback Leather Velvet Tulle Bow Pumps", "30", "Oja/public/img/bags/shoes/jimmy-choo-women-dark-blue-slingback-leather-velvet-tulle-bow-pumps.jpg","
+(29,"Jimmy Choo Dark Blue Slingback Leather Velvet Tulle Bow Pumps", "300", "Oja/public/img/bags/shoes/jimmy-choo-women-dark-blue-slingback-leather-velvet-tulle-bow-pumps.jpg","
 - US 7.5
 Heel Height: 12 cm
 Material: Leather, Velvet
 
 Light scuff on the heel. Good condition. Please see pictures.
 ", true, 2, 4),
-(30,"Jimmy Choo Leopard Pumps", "30", "Oja/public/img/bags/shoes/jimmy-choo-leopard-pumps.jpg","
+(30,"Jimmy Choo Leopard Pumps", "300", "Oja/public/img/bags/shoes/jimmy-choo-leopard-pumps.jpg","
 - US 6.5
 Leopard Pumps
 ", true, 2, 2),
-(31,"Black + Mugler 100 Mesh Boots by Jimmy Choo", "50", "Oja/public/img/bags/shoes/jimmy-choo-black-mugler-100-two-tone-jersey-and-mesh-bootsbooties.jpg","
+(31,"Black + Mugler 100 Mesh Boots by Jimmy Choo", "500", "Oja/public/img/bags/shoes/jimmy-choo-black-mugler-100-two-tone-jersey-and-mesh-bootsbooties.jpg","
 - US 8
 They're set on 100mm stiletto heels and have an elegant pointed toe, which will make your legs appear longer.
 
@@ -126,17 +126,17 @@ Pull on
 Black + Mugler 100 ' Two-tone Jersey' and Mesh Boots/Booties
 Size: EU 38 (Approx. US 8) Regular (M, B)
 ", true, 2, 5),
-(32,"Gucci Womens Sneakers", "30", "Oja/public/img/bags/shoes/gucci-womens-sneakers.jpg","
+(32,"Gucci Womens Sneakers", "500", "Oja/public/img/bags/shoes/gucci-womens-sneakers.jpg","
 - US 9
 Famous butterly Gucci Sneakers
 This item has visible signs of wear
 ", true, 2, 4),
-(33,"White Ace Bees Sneakers", "30", "/home/labber/lighthouse/finalProject/Oja/public/img/bags/shoes/gucci-white-ace-bees-sneakers.jpg","
+(33,"White Ace Bees Sneakers", "300", "/home/labber/lighthouse/finalProject/Oja/public/img/bags/shoes/gucci-white-ace-bees-sneakers.jpg","
 - US 7.5
 Famous butterly Gucci Sneakers
 Scuffs on Shoes 
 ", true, 2, 2),
-(34,"Brown Gg Canvas Monogram Lisa Mid Heel", "30", "Oja/public/img/bags/shoes/gucci-brown-gg-canvas-monogram-lisa-mid-heel-65eu-beige-ebony-bootsbooties.jpg","
+(34,"Brown Gg Canvas Monogram Lisa Mid Heel", "400", "Oja/public/img/bags/shoes/gucci-brown-gg-canvas-monogram-lisa-mid-heel-65eu-beige-ebony-bootsbooties.jpg","
 - US 6.5
 100% Authentic
 Brand new with tag and dust bag
@@ -148,14 +148,14 @@ Heel: 3.25 in
 Shaft: 17.5 in 
 Designer ID#: 551149 
 ", true, 2, 4),
-(35,"Pink Loubigirl 100mm Sandals", "30", "/home/labber/lighthouse/finalProject/Oja/public/img/bags/shoes/christian-louboutin-pink-loubigirl-100mm-sandals.jpg","
+(35,"Pink Loubigirl 100mm Sandals", "350", "/home/labber/lighthouse/finalProject/Oja/public/img/bags/shoes/christian-louboutin-pink-loubigirl-100mm-sandals.jpg","
 - US 10
 New Collection SS22
 Come with a box and a dust bag.
 Retail price $795 ++ tax.
 The Loubigirl sandal has elegant lines that reveal the foot for a sophisticated style that embodies Maison Christian Louboutin creative prowess. Made entirely from a dynamic Holly Pink satin, this sandal combines a carefree summer style with an elegant silhouette. Fastened by a slim strap with discrete finishes and mounted on a bold 100 mm heel, this model's originality comes from a thin choker-style crossover ankle strap.
 ", true, 2, 5),
-(36,"Multicolor Spikaqueen Louboutin Pumps ", "30", "/home/labber/lighthouse/finalProject/Oja/public/img/bags/shoes/christian-louboutin-multicolor-spikaqueen-pumps.jpg","
+(36,"Multicolor Spikaqueen Louboutin Pumps ", "400", "/home/labber/lighthouse/finalProject/Oja/public/img/bags/shoes/christian-louboutin-multicolor-spikaqueen-pumps.jpg","
 - US 11
 Never worn.
 The Spikaqueen sandal focuses on a sophisticated and sleek design, and is notable for its transparency. It is mounted on a 100 mm heel and has a thin choker-style ankle strap and PVC upper with a pointed strass embellished toe. An iridescent silver leather shoe that epitomises Maison Christian Louboutin know-how.
