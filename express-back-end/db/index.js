388f1745623e6ffa5b3f07b8dd42ab06b5536261
PandaBurglar <zane.bokhari@gmail.com>
2022-02-17 16:17:59 -0500

    new place for DB

--- express-back-end/db/index.js
+++ express-back-end/db/index.js
@@ -5,22 +5,44 @@ const Pool = require("pg").Pool;
 // IS THIS FILE NECESSARY IF DB_CONNECTIONS FILE EXISTS 
 
 
-const client = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT
-});
-// const client = new pg.Client({
-//   connectionString: process.env.DATABAS_URL || "",
-//   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+// const client = new Pool({
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT
 // });
-// console.log(process.env.DATABASE_URL);
+// // const client = new pg.Client({
+// //   connectionString: process.env.DATABAS_URL || "",
+// //   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
+// // });
+// // console.log(process.env.DATABASE_URL);
 
-client
-  .connect()
-  .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
+// client
+//   .connect()
+//   .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
 
 
-module.exports = client;
+// module.exports = client;
+
+//DB
+const createConnection= () => {
+
+  const client = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT
+  })
+  console.log(process.env)
+  client.connect()
+  
+  client.query('SELECT NOW()', (err, res) => (
+    console.log(err, res)
+  ))
+
+  return client;
+};
+
+exports.createConnection = createConnection;
