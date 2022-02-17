974f97654fa1b8cad1f7c06f6395fd1e1d858674
PandaBurglar <zane.bokhari@gmail.com>
2022-02-17 00:46:17 -0500

    remove attempt at backend user authorication, too complicated following example

--- express-back-end/routes/register.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.router(); 
-const registerController = require('Oja/express-back-end/controllers/registerController.js');
-
-router.post('/', registerController.handleNewUser );
-
-
-module.exports = router;
- 
