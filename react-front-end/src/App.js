e3c2b0fd9fdb0732b17ba7e95e9709d55fc9a826
Georgina Bada <georginabada@gmail.com>
2022-02-17 00:57:02 -0500

    Creating sliding images for homepage

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Register from './components/Register';
 import {Route, Link, Routes} from "react-router-dom";
+import ImageSlider from "./components/Slider/ImageSlider"
+import { SlideData } from './components/Slider/SliderData';
 
 
 function App(){
@@ -10,7 +12,7 @@ function App(){
   return (
     <main className="App">
       <Navbar/>
-    
+    <ImageSlider slides={SlideData}/>
        <Routes>
       <Route path="/register" element={<Register/>}>
       </Route>
