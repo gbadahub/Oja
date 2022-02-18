08270a6cfb720041058a62e3169ef72f4d3d8a93
Georgina Bada <georginabada@gmail.com>
2022-02-17 21:22:36 -0500

    Creating rent component

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -4,19 +4,26 @@ import Navbar from "./components/Navbar/Navbar";
 import Register from "./components/Register";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import Rent from "./components/Rent";
 import { Route, Link, Routes } from "react-router-dom";
-
 
 function App() {
   return (
     <main className="App">
       <Navbar />
+
+      <Routes>
+        <Route path="/rent" element={<Rent />}></Route>
+      </Routes>
+      
       <Routes>
         <Route path="/register" element={<Register />}></Route>
       </Routes>
+
       <Routes>
         <Route path="/login" element={<Login />}></Route>
       </Routes>
+
       <Routes>
         <Route path="/home" element={<Home />}></Route>
       </Routes>
