7b3a9d3ba28bca6edfc65c4c147b07e700313dd6
PandaBurglar <zane.bokhari@gmail.com>
2022-02-20 22:14:54 -0500

    fix homepage for redirect

--- react-front-end/src/components/Home.js
+++ react-front-end/src/components/Home.js
@@ -1,15 +1,11 @@
 import React from "react";
 import ImageSlider from "./Slider/ImageSlider";
 import { SlideData } from "./Slider/SliderData";
-import {useNavigate} from "react-router-dom";
 import axios from "axios";
 
 function Home (){
-  let navigate = useNavigate();
   axios.get("http://localhost:8080/api/homepage", { })
       .then(response =>{
-        // navigate('/api/homepage');
-        // useNavigate()('/homepage');
         console.log(response); 
       })
       .catch(error =>{
