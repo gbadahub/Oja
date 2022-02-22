77a6fe0f66ce44fe511b8cd8756b0e7b78580c60
Rofiat Olusanya <rofiatolusanya12@gmail.com>
2022-02-22 22:58:38 +0000

    Created a slider function to filter by price for item page

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -13,10 +13,7 @@ import Clothing from "./components/categories/Clothing";
 import ClothingItemPage from "./components/DetailItemPage/Clothingdetail/ClothingItem"
 import Accessories from "./components/categories/Accessories";
 import AccessoriesItemPage from "./components/DetailItemPage/Accessoriesdetails/AccessoriesItem";
-<<<<<<< HEAD
-import Cart from "./components/Cart";
-=======
->>>>>>> 2d410e4f625b4d5f80d8ba553aef64a9773ca871
+//import Cart from "./components/Cart";
 import { Route, Routes } from "react-router-dom";
 
 
@@ -30,7 +27,7 @@ function App() {
       <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} loginAuth={loginAuth} setLoginAuth={setLoginAuth}/>
 
 
-<Cart/>
+
       <Routes>
         <Route path="/bags" element={<Bags />}></Route>
       </Routes>
